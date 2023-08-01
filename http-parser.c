2555ec58f128df795c6e5e6463b6e49cc04a94a0
teddyrandby <tedrandby@gmail.com>
2023-08-01 17:53:30 +0000

    Update to return status

--- http-parser.c
+++ http-parser.c
@@ -5,8 +5,10 @@
 void gab_lib_parse(gab_engine *gab, gab_vm *vm, u8 argc, gab_value argv[argc]) {
   gab_value result = GAB_VAL_NIL();
 
-  if (argc != 1 && GAB_VAL_IS_STRING(argv[0]))
-    goto fin;
+  if (argc != 1) {
+    gab_push(vm, GAB_STRING("INVALID_ARGUMENTS"));
+    return;
+  }
 
   gab_obj_string *req = GAB_VAL_TO_STRING(argv[0]);
 
@@ -21,8 +23,10 @@ void gab_lib_parse(gab_engine *gab, gab_vm *vm, u8 argc, gab_value argv[argc]) {
                                &method_len, &path, &path_len, &minor_version,
                                headers, &num_headers, 0);
 
-  if (pret <= 0)
-    goto fin;
+  if (pret <= 0) {
+    gab_push(vm, GAB_STRING("COULD_NOT_PARSE"));
+    return;
+  }
 
   gab_obj_string *m =
       gab_obj_string_create(gab, s_i8_create((i8 *)method, method_len));
@@ -30,23 +34,19 @@ void gab_lib_parse(gab_engine *gab, gab_vm *vm, u8 argc, gab_value argv[argc]) {
   gab_obj_string *p =
       gab_obj_string_create(gab, s_i8_create((i8 *)path, path_len));
 
-  gab_value header_names[100];
+  s_i8 header_names[100];
   gab_value header_values[100];
 
   for (u32 i = 0; i < num_headers; i++) {
-    s_i8 header_name = s_i8_create((i8 *)headers[i].name, headers[i].name_len);
-    header_names[i] = GAB_VAL_OBJ(gab_obj_string_create(gab, header_name));
+    header_names[i] = s_i8_create((i8 *)headers[i].name, headers[i].name_len);
 
     s_i8 header_value =
         s_i8_create((i8 *)headers[i].value, headers[i].value_len);
     header_values[i] = GAB_VAL_OBJ(gab_obj_string_create(gab, header_value));
   }
 
-  gab_value headers_shape =
-      GAB_VAL_OBJ(gab_obj_shape_create(gab, vm, num_headers, 1, header_names));
-
-  gab_value headers_record = GAB_VAL_OBJ(gab_obj_record_create(
-      gab, vm, GAB_VAL_TO_SHAPE(headers_shape), 1, header_values));
+  gab_value headers_record =
+      gab_record(gab, vm, num_headers, header_names, header_values);
 
   s_i8 body = s_i8_create(req->data + pret, req->len - pret);
 
@@ -55,15 +55,13 @@ void gab_lib_parse(gab_engine *gab, gab_vm *vm, u8 argc, gab_value argv[argc]) {
   }
 
   gab_value values[] = {
+      GAB_STRING("ok"),
       GAB_VAL_OBJ(m),
       GAB_VAL_OBJ(p),
       headers_record,
   };
 
-  gab_push(vm, LEN_CARRAY(values), values);
-
-fin:
-  gab_push(vm, 1, &result);
+  gab_vpush(vm, LEN_CARRAY(values), values);
 }
 
 gab_value gab_mod(gab_engine *gab, gab_vm *vm) {
